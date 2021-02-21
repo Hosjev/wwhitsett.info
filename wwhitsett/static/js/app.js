066828f83461dea5027cd85b3b0a5094b548f202
Hosjev <hosjev@gmail.com>
2021-02-21 12:25:10 -0800

    cleaned up JS redundancy meeting NR

--- wwhitsett/static/js/app.js
+++ wwhitsett/static/js/app.js
@@ -1,15 +1,24 @@
-const pastInfo = document.querySelector("#past-info");
-const modal = document.querySelector("#inner-modal");
+import get from "./utils/getElement.js";
 
 // load some results after DOM
 window.addEventListener("DOMContentLoaded", () => {
-    // add listener to info bubble
-    pastInfo.addEventListener("click", () => {
-        // drawModal();
-        const closeModalBtn = document.querySelector(".modal-close")
-        modal.style.display = "block";
-        closeModalBtn.addEventListener("click", () => {
-            modal.style.display = "none";
-        })
-    })
+    // add listener to info bubble and study list
+    loadModal("past");
+    loadModal("study");
 });
+
+
+const loadModal = (element) => {
+    // elements unique to modal: name--study/past; btn; actual modal
+    const elementLink = get(`#${element}-info`);
+    const modalDisplay = get(`#${element}-modal`);
+    const modalCloseBtn = get(`.${element}-close`);
+    // add listener for modal display
+    elementLink.addEventListener("click", () => {
+        modalDisplay.style.display = "block";
+    });
+    // add listener for specific modal close
+    modalCloseBtn.addEventListener("click", () => {
+        modalDisplay.style.display = "none";
+    })
+}
