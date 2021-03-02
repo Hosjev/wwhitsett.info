df4197f9a3799a3d956ee8d28a82da624f3b0336
Hosjev <hosjev@gmail.com>
2021-03-01 18:18:01 -0800

    added local settings to django, removed trash

--- wwhitsett/settings.py
+++ wwhitsett/settings.py
@@ -132,3 +132,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 MEDIA_URL = '/media/'
+
+# Load local settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
